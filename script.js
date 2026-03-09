65819c972efa4d4e14fb34cec1f14e60e8a866b7
Kevin Ryan <kryanferreira@gmail.com>
2026-03-09 02:49:39 -0300

    lógica sidebar em css

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 "use strict";
 
-const sidebar = document.getElementById("sidebar");
-const openBtn = document.getElementById("open-btn");
-const menuItems = document.querySelectorAll(".side-item");
-const currentPage = window.location.pathname.split("/").pop();
-
-if (localStorage.getItem("sidebarOpen") === "true") {
-  sidebar.classList.add("open-sidebar");
-}
-
-openBtn.addEventListener("click", function () {
-  sidebar.classList.toggle("open-sidebar");
-
-  const isOpen = sidebar.classList.contains("open-sidebar");
-  localStorage.setItem("sidebarOpen", isOpen);
-});
-
-const selectLink = function () {
-  menuItems.forEach((item) => item.classList.remove("active"));
-  this.classList.add("active");
-};
-
-menuItems.forEach((item) => item.addEventListener("click", selectLink));
-
-menuItems.forEach((item) => {
-  const link = item.querySelector("a");
-  const linkPage = link.getAttribute("href");
-
-  if (linkPage === currentPage) {
-    item.classList.add("active");
-  }
+document.getElementById("open-btn").addEventListener("click", () => {
+  document.getElementById("sidebar").classList.toggle("open-sidebar");
 });
